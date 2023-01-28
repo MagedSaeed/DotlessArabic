0640ae010eb65421e7d3abda100bf6e120b26426
MagedSaeed <mageedsaeed1@gmail.com>
2023-01-28 22:50:42 +0300

    update data collecting for poems_dataset

--- dotless_arabic/experiments/nlms/poems_dataset/collect.py
+++ dotless_arabic/experiments/nlms/poems_dataset/collect.py
@@ -53,57 +53,71 @@ def collect_dataset(log_steps=True):
         "شعر التفعيلة",
         "شعر حر",
         "عدة أبحر",
+        "عامي",
+        # None,
     ]
+
+    if log_steps:
+        log_to_file(
+            text=f"""
+            Number datasets samples:
+            {len(ashaar)}
+            """,
+            results_file=dotted_results_file_path,
+        )
+
     ashaar = ashaar.filter(
         lambda example: example["poem meter"] not in non_accepted_meters
     )
 
-    poems = list()
+    if log_steps:
+        log_to_file(
+            text=f"""
+            Number datasets samples after filtering non accepted meters:
+            {len(ashaar)}
+            """,
+            results_file=dotted_results_file_path,
+        )
+
+    baits = list()
 
     for poem in tqdm(ashaar["poem verses"]):
-        clean_poem = list()
+        index = 1
         for shatr in poem:
-            processed_shatr = process(shatr)
-            if (
-                len(processed_shatr.replace(" ", "")) < 10
-                or len(processed_shatr.replace(" ", "")) > 30
-            ):
-                continue
-            if len(processed_shatr) > 0:
-                clean_poem.append(processed_shatr)
-        if clean_poem:
-            poems.append(clean_poem)
+            if index % 2 == 0:
+                baits.append(f"{prev_shatr} {shatr}")
+            else:
+                prev_shatr = shatr
+            index += 1
 
     if log_steps:
         log_to_file(
             text=f"""
             Sample of datasets samples:
-            {constants.NEW_LINE.join(poems[0][:5])}
+            {constants.NEW_LINE.join(baits[:5])}
             """,
             results_file=dotted_results_file_path,
         )
 
-    log_to_file(
-        text=f"""
-        Number of Poems:
-        {len(poems):,}
-        """,
-        results_file=dotted_results_file_path,
-    )
+        log_to_file(
+            text=f"""
+            Number of Baits:
+            {len(baits):,}
+            """,
+            results_file=dotted_results_file_path,
+        )
 
-    baits = list()
-    for poem in tqdm(poems):
-        bait = ""
-        for i, shatr in enumerate(poem, start=1):
-            bait += f"{shatr} "
-            if i % 2 == 0:
-                baits.append(bait.strip())
-                bait = ""
+    baits = list(
+        filter(
+            lambda bait: 60 >= len(process(bait).replace(" ", "")) >= 30,
+            tqdm(baits),
+        )
+    )
 
     if log_steps:
         log_to_file(
             text=f"""
-            Number of baits after deleting 30>= len(shatr) chars >= 10 chars:
+            Number of baits after deleting 60>= len(bait) chars >= 30 chars:
             {len(baits):,}
             """,
             results_file=dotted_results_file_path,
