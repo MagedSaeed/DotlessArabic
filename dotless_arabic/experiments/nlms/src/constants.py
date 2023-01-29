b8e830460e430dbe442578a0a7c444eebfd10845
MagedSaeed <mageedsaeed1@gmail.com>
2023-01-29 16:17:19 +0300

    update models params

--- dotless_arabic/experiments/nlms/src/constants.py
+++ dotless_arabic/experiments/nlms/src/constants.py
@@ -1,20 +1,20 @@
-import tkseem as tk
 import torch
 
 from dotless_arabic.constants import *
+
 
 DEFAULT_TOKENIZER_CLASS = "WordTokenizer"
 
 NUM_LAYERS = 4  # for GRU
 
-
 MAX_EPOCHS = 100
 BATCH_SIZE = 256
-HIDDEN_SIZE = 400
+HIDDEN_SIZE = 512
 DROPOUT_PROB = 0.333
-EMBEDDING_SIZE = 400
+EMBEDDING_SIZE = 512
 LEARNING_RATE = 0.001
 DEFAULT_VOCAB_COVERAGE = 0.95  # for tokenizer to consider only tokens that cover this value of the running text
+SEQUENCE_LENGTH_PERCENTILE = 0.99  # percentile to consider lengths and return up to that percentile. 0.5 percentile should return the average
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LIGHTING_ACCELERATOR = "auto"  # used by pytorch_lightning
 
