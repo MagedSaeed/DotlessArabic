55c11e537aa76de8eaaa72563ed761d7a5063509
MagedSaeed <mageedsaeed1@gmail.com>
2023-01-29 15:19:05 +0300

    fix issues in logging files

--- dotless_arabic/datasets/aggregated/collect.py
+++ dotless_arabic/datasets/aggregated/collect.py
@@ -6,52 +6,43 @@ if "." not in sys.path:
 
 from pathlib import Path
 
-from dotless_arabic.datasets.news_dataset.collect import (
+from dotless_arabic.datasets.news.collect import (
     collect_dataset as collect_news_dataset,
 )
-from dotless_arabic.datasets.poems_dataset.collect import (
+from dotless_arabic.datasets.poems.collect import (
     collect_dataset as collect_poems_dataset,
 )
-from dotless_arabic.datasets.quran_dataset.collect import (
+from dotless_arabic.datasets.quran.collect import (
     collect_dataset as collect_quran_dataset,
 )
-from dotless_arabic.datasets.sanadset_hadeeth_dataset.collect import (
+from dotless_arabic.datasets.sanadset_hadeeth.collect import (
     collect_dataset as collect_hadeeth_dataset,
 )
 from dotless_arabic.experiments.nlms.src import constants
 from dotless_arabic.experiments.nlms.src.utils import log_to_file
-from dotless_arabic.datasets.wikipedia_dataset.collect import (
+from dotless_arabic.datasets.wikipedia.collect import (
     collect_dataset as collect_wikipedia_dataset,
 )
 from dotless_arabic.processing import dataset_dot_transform, dataset_newline_transform
 
 
-def collect_dataset(log_steps=True):
-
-    if log_steps:
-        current_dir = Path(__file__).resolve().parent
-
-        dotted_results_file_path = f"{current_dir}/results_dotted.txt"
-
-        # delete the current logging file, if exists
-
-        Path(dotted_results_file_path).unlink(missing_ok=True)
+def collect_dataset(results_file=None):
 
     dataset = (
-        collect_quran_dataset(log_steps=False)
-        + collect_hadeeth_dataset(log_steps=False)
-        + collect_poems_dataset(log_steps=False)
-        + collect_news_dataset(log_steps=False)
-        + collect_wikipedia_dataset(log_steps=False)
+        collect_quran_dataset()
+        + collect_hadeeth_dataset()
+        + collect_poems_dataset()
+        + collect_news_dataset()
+        + collect_wikipedia_dataset()
     )
 
-    if log_steps:
+    if results_file is not None:
         log_to_file(
             text=f"""
             Sample of datasets samples:
             {constants.NEW_LINE.join(dataset[:2])}
             """,
-            results_file=dotted_results_file_path,
+            results_file=results_file,
         )
 
         log_to_file(
@@ -59,20 +50,20 @@ def collect_dataset(log_steps=True):
             Number of Samples before transformations:
             {len(dataset):,}
             """,
-            results_file=dotted_results_file_path,
+            results_file=results_file,
         )
 
     # just to double check
 
     dataset = dataset_dot_transform(dataset_newline_transform(dataset))
 
-    if log_steps:
+    if results_file is not None:
         log_to_file(
             text=f"""
             Number of Samples after transformations:
             {len(dataset):,}
             """,
-            results_file=dotted_results_file_path,
+            results_file=results_file,
         )
 
     return dataset
--- dotless_arabic/datasets/wikipedia/collect.py
+++ dotless_arabic/datasets/wikipedia/collect.py
@@ -1,19 +1,11 @@
-import os
 import sys
-from pathlib import Path
-
-import datasets
-import pandas as pd
 from tqdm.auto import tqdm
 
 if "." not in sys.path:
     sys.path.append(".")
 
 
-import datasets
-
 from dotless_arabic.experiments.nlms.src import constants
-from dotless_arabic.experiments.nlms.src.training_pipeline import training_pipeline
 from dotless_arabic.experiments.nlms.src.utils import log_to_file
 from dotless_arabic.processing import (
     dataset_dot_transform,
@@ -21,58 +13,52 @@ from dotless_arabic.processing import (
 )
 
 
-def collect_dataset(log_steps=True):
-
-    if log_steps:
-        current_dir = Path(__file__).resolve().parent
-
-        dotted_results_file_path = f"{current_dir}/results_dotted.txt"
-
-        # delete the current logging file, if exists
-
-        Path(dotted_results_file_path).unlink(missing_ok=True)
+def collect_dataset(results_file=None):
 
     with open("../wikipedia_dataset.txt", "r") as news_dataset_file:
         wikipedia_dataset = news_dataset_file.read().splitlines()
 
     dataset = wikipedia_dataset
 
-    log_to_file(
-        text=f"""
-        Sample of datasets samples:
-        {constants.NEW_LINE.join(dataset[:2])}
-        """,
-        results_file=dotted_results_file_path,
-    )
+    if results_file is not None:
+        log_to_file(
+            text=f"""
+            Sample of datasets samples:
+            {constants.NEW_LINE.join(dataset[:2])}
+            """,
+            results_file=results_file,
+        )
 
-    log_to_file(
-        text=f"""
-        Number of Samples before transformations:
-        {len(dataset):,}
-        """,
-        results_file=dotted_results_file_path,
-    )
+        log_to_file(
+            text=f"""
+            Number of Samples before transformations:
+            {len(dataset):,}
+            """,
+            results_file=results_file,
+        )
 
     dataset = dataset_dot_transform(dataset_newline_transform(dataset))
 
-    log_to_file(
-        text=f"""
-        Number of Samples after transformations:
-        {len(dataset):,}
-        """,
-        results_file=dotted_results_file_path,
-    )
+    if results_file is not None:
+        log_to_file(
+            text=f"""
+            Number of Samples after transformations:
+            {len(dataset):,}
+            """,
+            results_file=results_file,
+        )
 
     # consider only those samples who have 10 tokens or more
 
     dataset = list(filter(lambda document: len(document.split()) >= 30, tqdm(dataset)))
 
-    log_to_file(
-        text=f"""
-        Number of Samples when considering sample with 30 tokens or more:
-        {len(dataset):,}
-        """,
-        results_file=dotted_results_file_path,
-    )
+    if results_file is not None:
+        log_to_file(
+            text=f"""
+            Number of Samples when considering sample with 30 tokens or more:
+            {len(dataset):,}
+            """,
+            results_file=results_file,
+        )
 
     return dataset
