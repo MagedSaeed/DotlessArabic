612b003699162c89e6bcc93fddbb7a272d6a9b85
MagedSaeed <mageedsaeed1@gmail.com>
2023-03-29 13:00:09 +0300

    collect dataset for meter classification task

--- dotless_arabic/datasets/poems/collect.py
+++ dotless_arabic/datasets/poems/collect.py
@@ -10,6 +10,38 @@ from dotless_arabic import constants
 from dotless_arabic.utils import log_content
 from dotless_arabic.processing import process
 
+NON_CLASSICAL_METERS = [
+    "التفعيله",
+    "الحداء",
+    "الدوبيت",
+    "السلسلة",
+    "الصخري",
+    "الكان كان",
+    "اللويحاني",
+    "المسحوب",
+    "المواليا",
+    "الموشح",
+    "الهجيني",
+    "بحر التفعيله",
+    "بحر الدوبيت",
+    "بحر السلسلة",
+    "بحر القوما",
+    "بحر المواليا",
+    "بحر تفعيلة الرجز",
+    "بحر تفعيلة الرمل",
+    "بحر تفعيلة الكامل",
+    "بحر تفعيلة المتقارب",
+    "بحر مجزوء الدوبيت",
+    "بحر مجزوء المواليا",
+    "بحر مخلع موشح",
+    "زجل",
+    "شعر التفعيلة",
+    "شعر حر",
+    "عدة أبحر",
+    "عامي",
+    # None,
+]
+
 
 def collect_dataset_for_analysis(results_file=None):
     ashaar = datasets.load_dataset("arbml/ashaar", split="train")
@@ -23,38 +55,6 @@ def collect_dataset_for_language_modeling(results_file=None):
 
     ashaar = datasets.load_dataset("arbml/ashaar", split="train")
 
-    non_accepted_meters = [
-        "التفعيله",
-        "الحداء",
-        "الدوبيت",
-        "السلسلة",
-        "الصخري",
-        "الكان كان",
-        "اللويحاني",
-        "المسحوب",
-        "المواليا",
-        "الموشح",
-        "الهجيني",
-        "بحر التفعيله",
-        "بحر الدوبيت",
-        "بحر السلسلة",
-        "بحر القوما",
-        "بحر المواليا",
-        "بحر تفعيلة الرجز",
-        "بحر تفعيلة الرمل",
-        "بحر تفعيلة الكامل",
-        "بحر تفعيلة المتقارب",
-        "بحر مجزوء الدوبيت",
-        "بحر مجزوء المواليا",
-        "بحر مخلع موشح",
-        "زجل",
-        "شعر التفعيلة",
-        "شعر حر",
-        "عدة أبحر",
-        "عامي",
-        # None,
-    ]
-
     log_content(
         content=f"""
         Number datasets samples:
@@ -64,7 +64,7 @@ def collect_dataset_for_language_modeling(results_file=None):
     )
 
     ashaar = ashaar.filter(
-        lambda example: example["poem meter"] not in non_accepted_meters
+        lambda example: example["poem meter"] not in NON_CLASSICAL_METERS
     )
 
     log_content(
@@ -118,3 +118,74 @@ def collect_dataset_for_language_modeling(results_file=None):
     )
 
     return baits
+
+
+METER_CLASSES = []
+
+
+def collect_dataset_for_language_modeling(results_file=None):
+
+    ashaar = datasets.load_dataset("arbml/ashaar", split="train")
+
+    log_content(
+        content=f"""
+        Number datasets samples:
+        {len(ashaar)}
+        """,
+        results_file=results_file,
+    )
+
+    ashaar = ashaar.filter(
+        lambda example: example["poem meter"] not in NON_CLASSICAL_METERS + [None]
+    )
+
+    log_content(
+        content=f"""
+        Number datasets samples after filtering non accepted meters:
+        {len(ashaar)}
+        """,
+        results_file=results_file,
+    )
+
+    baits_with_classes = dict()
+
+    for poem, meter in tqdm(zip(ashaar["poem verses"], ashaar["poem meter"])):
+        index = 1
+        for shatr in poem:
+            if index % 2 == 0:
+                baits_with_classes[f"{prev_shatr} {shatr}"] = meter
+            else:
+                prev_shatr = shatr
+            index += 1
+
+    log_content(
+        content=f"""
+        Sample of datasets samples:
+        {constants.NEW_LINE.join(list(baits_with_classes.keys())[:5])}
+        """,
+        results_file=results_file,
+    )
+
+    log_content(
+        content=f"""
+        Number of Baits:
+        {len(baits_with_classes.keys()):,}
+        """,
+        results_file=results_file,
+    )
+
+    baits_with_classes = {
+        bait: meter
+        for bait, meter in baits_with_classes.items()
+        if 60 >= len(process(bait).replace(" ", "")) >= 30
+    }
+
+    log_content(
+        content=f"""
+        Number of baits after deleting 60>= len(bait) chars >= 30 chars:
+        {len(baits_with_classes.keys()):,}
+        """,
+        results_file=results_file,
+    )
+
+    return baits_with_classes
