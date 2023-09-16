08e9483b149907c5d6e6544cbfb501bc576ecf99
MagedSaeed <mageedsaeed1@gmail.com>
2023-09-16 21:16:16 +0300

    add sentiment analysis task

---
+++ dotless_arabic/experiments/sentiment_analysis/src/settings.py
@@ -0,0 +1,19 @@
+import os
+import nltk
+import random
+
+import torch
+
+from pytorch_lightning import seed_everything
+
+from dotless_arabic.experiments.meter_classification.src import constants
+
+
+def configure_environment():
+    random.seed(42)
+    nltk.download("stopwords")
+    os.environ["WANDB_MODE"] = "disabled"
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"  # to see CUDA errors
+    torch.cuda.empty_cache()  # to free gpu memory
+    seed_everything(42, workers=True)
+    # other options: https://stackoverflow.com/questions/15197286/how-can-i-flush-gpu-memory-using-cuda-physical-reset-is-unavailable
