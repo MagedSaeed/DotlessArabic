913540f28d913bdadd52b60bc0e8c2eea6c48e0d
Maged Saeed <mageedsaeed1@gmail.com>
2023-09-20 19:43:23 +0300

    adding best_params file template for exps

---
+++ dotless_arabic/experiments/topic_modeling/src/best_params.py
@@ -0,0 +1,32 @@
+best_params = {
+    "sanad": {
+        "WordTokenization": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=256,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
+        # "FarasaMorphologicalTokenizer": dict(
+        #     num_layers=2,
+        #     learning_rate=0.001,
+        #     hidden_size=256,
+        #     embedding_size=512,
+        #     dropout_prop=0.2,
+        # ),
+        # "DisjointLetterTokenizer": dict(
+        #     num_layers=2,
+        #     learning_rate=0.001,
+        #     hidden_size=256,
+        #     embedding_size=512,
+        #     dropout_prop=0.2,
+        # ),
+        # "CharacterTokenizer": dict(
+        #     num_layers=2,
+        #     learning_rate=0.001,
+        #     hidden_size=256,
+        #     embedding_size=512,
+        #     dropout_prop=0.2,
+        # ),
+    },
+}
