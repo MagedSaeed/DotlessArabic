0be9b0ba1e54f57237487b1a01dc6816987449bd
Maged Saeed <mageedsaeed1@gmail.com>
2023-09-21 13:14:29 +0300

    fix  bug in epoch train time callback

--- dotless_arabic/callbacks.py
+++ dotless_arabic/callbacks.py
@@ -8,10 +8,10 @@ class EpochTimerCallback(Callback):
         self.epochs_times = list()
         self.average_epochs_time = 0
 
-    def on_epoch_start(self, trainer, pl_module):
+    def on_train_epoch_start(self, trainer, pl_module):
         self.start_time = time.time()
 
-    def on_epoch_end(self, trainer, pl_module):
+    def on_train_epoch_end(self, trainer, pl_module):
         end_time = time.time()
         epoch_time = end_time - self.start_time
         self.epochs_times.append(epoch_time)
@@ -19,6 +19,6 @@ class EpochTimerCallback(Callback):
     def on_train_end(self, trainer, pl_module):
         self.average_epochs_time = sum(self.epochs_times) / len(self.epochs_times)
         pl_module.log(
-            name="per-epoch-time",
+            name="per-epoch-avg-time",
             value=self.average_epochs_time,
         )
