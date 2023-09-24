e02264526e3907391640120f5675670c8d40ffa4
MagedSaeed <mageedsaeed1@gmail.com>
2023-09-24 22:59:46 +0300

    remove unnecessary vars from constants

--- dotless_arabic/experiments/sentiment_analysis/src/constants.py
+++ dotless_arabic/experiments/sentiment_analysis/src/constants.py
@@ -2,29 +2,20 @@ import torch
 
 from dotless_arabic.experiments.constants import *
 
-from dotless_arabic.tokenizers import CharacterTokenizer
+from dotless_arabic.tokenizers import WordTokenizer
 
-
-GRU_LAYERS = 5  # for GRU
-GRU_DROPOUT = 0.25
 
 MAX_EPOCHS = 50
 DEFAULT_BATCH_SIZE = 128
-HIDDEN_SIZE = 256
-EMBEDDING_SIZE = 512
-LEARNING_RATE = 0.001
 DEFAULT_VOCAB_COVERAGE = 0.9
 
-SEQUENCE_LENGTH_PERCENTILE = 0.95
+SEQUENCE_LENGTH_PERCENTILE = 0.8
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LIGHTING_ACCELERATOR = "auto"  # used by pytorch_lightning
-
-TEST_SIZE = 0.1
-VAL_SIZE = 0.05
 
 GPU_DEVICES = "0"  # consider only one CPU core if there is no GPU
 
 CPU_DEVICES = 1  # I tried with higher values but this did not work
 
-DEFAULT_TOKENIZER_CLASS = CharacterTokenizer
+DEFAULT_TOKENIZER_CLASS = WordTokenizer
