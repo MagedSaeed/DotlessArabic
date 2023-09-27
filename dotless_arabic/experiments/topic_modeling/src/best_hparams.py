3ad1ce614d68a5c72ea13f96662e57ff77c7add8
Maged Saeed <mageedsaeed1@gmail.com>
2023-09-27 10:35:44 +0300

    adding best hparams for tm

--- dotless_arabic/experiments/topic_modeling/src/best_hparams.py
+++ dotless_arabic/experiments/topic_modeling/src/best_hparams.py
@@ -1,11 +1,11 @@
 best_params = {
     "sanad": {
         "WordTokenizer": dict(
-            # num_layers=2,
-            # learning_rate=0.001,
-            # hidden_size=256,
-            # embedding_size=512,
-            # dropout_prop=0.2,
+            rnn_hiddens=128,
+            rnn_dropout=0.5,
+            dropout_prob=0.5,
+            embedding_size=128,
+            learning_rate=0.001,
         ),
         # "FarasaMorphologicalTokenizer": dict(
         #     num_layers=2,
