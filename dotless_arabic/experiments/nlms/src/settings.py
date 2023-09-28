549e5a27003f9bc55e01180be20c93a1c25eb72c
MagedSaeed <mageedsaeed1@gmail.com>
2023-09-28 22:28:01 +0300

    comment wandb_mod env var

--- dotless_arabic/experiments/nlms/src/settings.py
+++ dotless_arabic/experiments/nlms/src/settings.py
@@ -10,6 +10,6 @@ def configure_environment(gpu_devices="0"):
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:2"
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_devices
-    os.environ["WANDB_MODE"] = "disabled"
+    # os.environ["WANDB_MODE"] = "disabled"
     seed_everything(constants.RANDOM_SEED, workers=True)
     wandb.login()
