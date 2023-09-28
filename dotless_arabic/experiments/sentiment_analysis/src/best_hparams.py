b33003ebe11672382d14f26d974be43e6d199226
Maged Saeed <mageedsaeed1@gmail.com>
2023-09-28 17:07:18 +0300

    add best hparams for farasa sa

--- dotless_arabic/experiments/sentiment_analysis/src/best_hparams.py
+++ dotless_arabic/experiments/sentiment_analysis/src/best_hparams.py
@@ -8,13 +8,14 @@ best_hparams = {
             embedding_size=512,
             learning_rate=0.01,
         ),
-        # "FarasaMorphologicalTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "FarasaMorphologicalTokenizer": dict(
+            num_layers=2,
+            rnn_hiddens=256,
+            rnn_dropout=0.25,
+            dropout_prob=0.5,
+            embedding_size=256,
+            learning_rate=0.001,
+        ),
         # "DisjointLetterTokenizer": dict(
         #     num_layers=2,
         #     learning_rate=0.001,
