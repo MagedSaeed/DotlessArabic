78d49c7e57df6fb111351c83a8f488061cfb9cda
Maged Saeed <mageedsaeed1@gmail.com>
2023-10-07 13:09:37 +0300

    fix issues with mask types

--- dotless_arabic/experiments/translation/src/models.py
+++ dotless_arabic/experiments/translation/src/models.py
@@ -43,39 +43,18 @@ class TokenEmbedding(nn.Module):
         self.emb_size = emb_size
 
     def forward(self, tokens: Tensor):
-        return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
 
-
-def generate_square_subsequent_mask(sz):
-    mask = (torch.triu(torch.ones((sz, sz), device=constants.DEVICE)) == 1).transpose(
-        0, 1
-    )
-    mask = (
-        mask.float()
-        .masked_fill(mask == 0, float("-inf"))
-        .masked_fill(mask == 1, float(0.0))
-    )
-    return mask
-
-
-def generate_square_subsequent_mask(sz):
-    mask = (torch.triu(torch.ones((sz, sz), device=constants.DEVICE)) == 1).transpose(
-        0, 1
-    )
-    mask = (
-        mask.float()
-        .masked_fill(mask == 0, float("-inf"))
-        .masked_fill(mask == 1, float(0.0))
-    )
-    return mask
-
-
-def create_mask(src, tgt, pad_idx):
+def create_masks(src, tgt, pad_idx):
     src_seq_len = src.shape[1]
     tgt_seq_len = tgt.shape[1]
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
-    src_mask = torch.zeros((src_seq_len, src_seq_len), device=constants.DEVICE).type(
-        torch.bool
+    tgt_mask = nn.Transformer.generate_square_subsequent_mask(
+        tgt_seq_len,
+        device=constants.DEVICE,
+    ).to(torch.bool)
+    src_mask = torch.zeros(
+        (src_seq_len, src_seq_len),
+        device=constants.DEVICE,
+        dtype=torch.bool,
     )
     src_padding_mask = (src == pad_idx).clone().detach()
     tgt_padding_mask = (tgt == pad_idx).clone().detach()
@@ -124,7 +103,7 @@ class TranslationTransformer(LightningModule):
         self.dense = nn.Linear(emb_size, tgt_vocab_size)
 
     def forward(self, src, trg):
-        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_masks(
             src,
             trg,
             pad_idx=self.pad_token_id,
