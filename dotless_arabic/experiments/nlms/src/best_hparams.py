739e8dd5d73f0f77d1d255fd018773e4f8ca057e
Maged Saeed <mageedsaeed1@gmail.com>
2023-11-23 22:02:38 +0300

    add missing hparams

--- dotless_arabic/experiments/nlms/src/best_hparams.py
+++ dotless_arabic/experiments/nlms/src/best_hparams.py
@@ -14,20 +14,20 @@ best_hparams = {
             embedding_size=512,
             dropout_prob=0.5,
         ),
-        # "DisjointLetterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
-        # "CharacterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "DisjointLetterTokenizer": dict(
+            num_layers=3,
+            learning_rate=0.01,
+            hidden_size=256,
+            embedding_size=512,
+            dropout_prop=0.3333,
+        ),
+        "CharacterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.01,
+            hidden_size=256,
+            embedding_size=512,
+            dropout_prop=0.3333,
+        ),
     },
     "sanadset_hadeeth": {
         "WordTokenizer": dict(
@@ -44,20 +44,20 @@ best_hparams = {
             embedding_size=512,
             dropout_prob=0.2,
         ),
-        # "DisjointLetterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
-        # "CharacterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "DisjointLetterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
+        "CharacterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
     },
     "poems": {
         "WordTokenizer": dict(
@@ -74,20 +74,20 @@ best_hparams = {
             embedding_size=256,
             dropout_prob=0.2,
         ),
-        # "DisjointLetterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
-        # "CharacterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "DisjointLetterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
+        "CharacterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
     },
     "wikipedia": {
         "WordTokenizer": dict(
@@ -104,20 +104,20 @@ best_hparams = {
             embedding_size=512,
             dropout_prob=0.2,
         ),
-        # "DisjointLetterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
-        # "CharacterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "DisjointLetterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=256,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
+        "CharacterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
     },
     "news": {
         "WordTokenizer": dict(
@@ -134,19 +134,19 @@ best_hparams = {
             embedding_size=512,
             dropout_prob=0.2,
         ),
-        # "DisjointLetterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
-        # "CharacterTokenizer": dict(
-        #     num_layers=2,
-        #     learning_rate=0.001,
-        #     hidden_size=256,
-        #     embedding_size=512,
-        #     dropout_prop=0.2,
-        # ),
+        "DisjointLetterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
+        "CharacterTokenizer": dict(
+            num_layers=2,
+            learning_rate=0.001,
+            hidden_size=512,
+            embedding_size=512,
+            dropout_prop=0.2,
+        ),
     },
 }
